ff7c4240e22ce8bb1726b0b4fabc7165c46da83e
Douglas K <douglaskrein@gmail.com>
2016-02-10 02:18:24 -0500

    changed to use the envoirment variable to the port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var port = process.env.PORT || 3000;
 var express = require('./config/express');
 var app = express();
 
-app.listen(3000);
+app.listen(port);
 module.exports = app;
 
 console.log('Server running at port 3000');
